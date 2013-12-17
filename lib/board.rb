c0fe5d62f4cf55db8c802d1677b0e2c7d8b28367
Kyle McKee <mckee.kyle@gmail.com>
2013-12-16 22:58:49 -0500

    Micro commit.

--- lib/board.rb
+++ lib/board.rb
@@ -7,30 +7,18 @@ class Board
     @board = board
   end
   def get_neighbor_values_for row_index, column_index
-    neighbors = []
+    previous_row = @board[row_index - 1] if row_index > 0
     selected_row = @board[row_index]
-    last_column = selected_row.length - 1
-    left_column = column_index - 1
-    right_column = column_index + 1
-    neighbors << selected_row[left_column] if left_column >= 0
-    neighbors << selected_row[right_column] if  right_column <= last_column 
-
-    previous_row = @board[row_index -1] if row_index > 0
-
-    if previous_row != nil
-      neighbors << previous_row[left_column] 
-      neighbors << previous_row[column_index]
-      neighbors << previous_row[right_column]
-    end
-
     next_row = @board[row_index + 1]
     
-    if next_row != nil
-      neighbors << next_row[left_column] if left_column >= 0
-      neighbors << next_row[column_index]
-      neighbors << next_row[right_column]
-    end
-    neighbors
+    left_column = column_index - 1
+    right_column = column_index + 1
+   
+    neighbors = []
+    neighbors << get_values_from_row(selected_row, left_column, right_column)
+    neighbors << get_values_from_row(previous_row, left_column, column_index, right_column)
+    neighbors << get_values_from_row(next_row, left_column, column_index, right_column)
+    neighbors.flatten!
   end
   def get_live_neighbor_count row_index, column_index
     get_neighbor_values_for(row_index, column_index).count { |value| value == "X" } 
@@ -44,6 +32,18 @@ class Board
       string += row.to_row_string
     end
     string
+  end
+
+  private 
+
+  def get_values_from_row row, *column_indexes
+    values = []
+    if row != nil
+      column_indexes.each do |index|
+        values << row[index] if index >= 0 && index < row.count
+      end
+    end
+    values
   end
 end
 
