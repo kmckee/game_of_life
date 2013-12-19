785f0d63a7c9ecc789fe54bc7d920d71db3be883
Kyle McKee <mckee.kyle@gmail.com>
2013-12-18 22:27:29 -0500

    Micro commit.

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -5,16 +5,7 @@ class GameOfLife
   def update
     new_board = []
     @current_board.each_with_index do |row,row_index|
-      new_row = []
-      row.each_with_index do |value, cell_index|
-        live_neighbors = @current_board.get_live_neighbor_count(row_index, cell_index) 
-        if (live_neighbors == 3 || live_neighbors == 2)
-          new_row << "X"
-        else
-          new_row << "."
-        end
-      end
-      new_board << new_row
+      new_board << get_new_row_for(row, row_index)
     end
     @current_board = Board.new new_board
   end
@@ -23,5 +14,19 @@ class GameOfLife
   end
   def board
     @current_board.to_s 
+  end
+
+  private
+
+  def get_new_row_for row, row_index
+    new_row = []
+    row.each_with_index do |value, cell_index|
+      new_row << get_new_value_for_cell_at(row_index, cell_index)
+    end
+    new_row
+  end
+  def get_new_value_for_cell_at x, y
+    live_neighbors = @current_board.get_live_neighbor_count(x, y) 
+    (live_neighbors == 3 || live_neighbors == 2) ? "X" : "."
   end
 end
